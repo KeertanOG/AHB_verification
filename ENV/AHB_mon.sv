135cea5b044c924cb695ce883ac3cb8498c85609
KeertanOG <keertanpatelrptp@gmail.com>
2025-05-15 22:04:49 +0530

    updated driver and monitor

--- ENV/AHB_mon.sv
+++ ENV/AHB_mon.sv
@@ -19,6 +19,7 @@ class AHB_mon;
 
   virtual AHB_inf.MON_MP vif;
   mailbox #(AHB_trans) mon2sb;
+  AHB_trans trans_h;
 
 
   function void connect(virtual AHB_inf.MON_MP mon_vif,
@@ -26,11 +27,46 @@ class AHB_mon;
     this.vif = mon_vif;
     this.mon2sb = mon2sb;
   endfunction
-  
+ task get_control_signals();              //samples the control signals
+   case(vif.mon_cb.hburst)
+     3'b000 : trans_h.burst = hburst_e.SINGLE;
+     3'b001 : trans_h.burst = hburst_e.INCR;
+     3'b010 : trans_h.burst = hburst_e.WRAP4;
+     3'b011 : trans_h.burst = hburst_e.INCR4;
+     3'b100 : trans_h.burst = hburst_e.WRAP8;
+     3'b101 : trans_h.burst = hburst_e.INCR8;
+     3'b110 : trans_h.burst = hburst_e.WRAP16;
+     3'b111 : trans_h.burst = hburst_e.INCR16;
+   endcase
+   trans_h.hsel = vif.mon_cb.sel;
+   trans_h.hwrite = vif.mon_cb.hwrite;
+   trans_h.hsize = vif.mon_cb.hsize;
+   trans_h.htrans = vif.mon_cb.htrans;
+   trans_h.haddr_que.push_back(vif.mon_cb.haddr);      //for single burst type or the first transfer of the burst type transfer
+
+//   if(vif.mon_cb.hburst != 3'b000) begin
+   if(trans_h.calc_txf > 1) begin
+     for(int i=0; i<trans_h.calc_txf; i++) begin
+       trans_h.htrans = vif.mon_cb.htrans;
+       trans_h.haddr_que.push_back(vif.mon_cb.haddr);
+     end
+   end
+ endtask
+
+ task get_data_signals();                 //samples the data signals
+ endtask
+
+ task get_from_dut();
+   fork
+     get_control_signals();
+     get_data_signals();
+   join_any
+ endtask
 
   task run();
-    //forever begin
-    //end
+    trans_h = new();
+//    forever begin
+//    end
   endtask
 
 endclass
