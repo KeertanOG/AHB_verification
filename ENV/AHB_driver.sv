2f609d27256b8d11b4ab60b21f882d08953d551c
KeertanOG <keertanpatelrptp@gmail.com>
2025-05-20 16:01:09 +0530

    changes made as sir told

--- ENV/AHB_driver.sv
+++ ENV/AHB_driver.sv
@@ -19,9 +19,9 @@ class AHB_driver;
 
   virtual AHB_inf.DRV_MP vif;             // AHB Driver Interface
   mailbox #(AHB_trans) gen2drv;
-  AHB_trans trans_h,trans_h2,trans_h3;
-  AHB_trans addr_phase_que[$];
-  AHB_trans data_phase_que[$];
+  AHB_trans trans_h;
+  AHB_trans addr_phase_que[$];            //queue for address phase data
+  AHB_trans data_phase_que[$];            //queue for data phase
   
   // Connect method
   function void connect(mailbox #(AHB_trans) gen2drv, virtual AHB_inf.DRV_MP vif);
@@ -29,64 +29,15 @@ class AHB_driver;
     this.gen2drv = gen2drv;
   endfunction
   
-  task drive_control_signals(); 				                    //drives control signals of AHB
-    
-    wait(addr_phase_que.size != 0);                         //checking if the addr phase data present or not
-    trans_h2=addr_phase_que.pop_front();
-    vif.drv_cb.hsel <= 1'b1;
-    vif.drv_cb.hwrite <= trans_h2.hwrite;
-    vif.drv_cb.hsize <= trans_h2.hsize;
-    vif.drv_cb.hburst <= int'(trans_h2.hburst_e);
-    vif.drv_cb.htrans <= trans_h2.htrans;                       //for single burst type or the first transfer of burst type transaction
-    vif.drv_cb.haddr <= trans_h2.haddr_que.pop_front();
-
-    if(trans_h2.calc_txf > 1) begin                             //for burst type transfers
-      for(int i = 0; i < trans_h2.calc_txf -1; i++) begin
-        vif.drv_cb.haddr <= trans_h2.haddr_que.pop_front();
-        vif.drv_cb.htrans <= 2'b11;                             //htrans = SEQ 
-        @(vif.drv_cb);
-      end
-    end
-  endtask
-
-  task drive_data_signals();                                //drives the data signals
-
-    wait(data_phase_que.size != 0);                         //checking if the data phase data present or not
-    trans_h3 = data_phase_que.pop_front();
-
-    if(trans_h3.hwrite)
-      vif.drv_cb.hwdata <= trans_h3.hwdata_que.pop_front();                 //for the single burst type or first transfer of the burst type transaction
-
-    if(trans_h3.calc_txf) begin
-      for(int i=0; i<trans_h3.calc_txf -1;i++) begin
-        if(trans_h3.hwrite)
-          vif.drv_cb.hwdata <= trans_h3.hwdata_que.pop_front();
-        @(vif.drv_cb);
-      end
-    end
-  endtask
-
-  task send_to_dut;
-    fork
-      @(vif.drv_cb) drive_control_signals();
-      begin 
-        repeat(2) @(vif.drv_cb);
-        drive_data_signals();
-      end
-    join_any
-  endtask
-
   task run();
     trans_h = new();
-    trans_h2 =new();
-    trans_h3 = new();
     fork
       repeat(20) begin
         gen2drv.get(trans_h);
-        trans_h.print("Driver");
+        //trans_h.print("Driver");
         addr_phase_que.push_back(trans_h);
-        data_phase_que.push_back(trans_h);
-        send_to_dut();
+        //data_phase_que.push_back(trans_h);
+        send_to_dut(trans_h);
         //trans_h.print("Driver");
       end
       //drive_control_io();                       //drives the control signals of AHB
@@ -94,6 +45,55 @@ class AHB_driver;
     join
   endtask
 
+  task send_to_dut(AHB_trans trans_h);
+    fork
+      drive_control_signals(trans_h);
+      drive_data_signals(trans_h);
+      end
+    join_any
+  endtask
+
+  task drive_control_signals(AHB_trans trans_h); 				                    //drives control signals of AHB
+    forever begin
+      wait(addr_phase_que.size != 0);                         //checking if the addr phase data present or not
+      trans_h=addr_phase_que.pop_front();
+      @(vif.drv_cb);
+      vif.drv_cb.hsel <= 1'b1;
+      vif.drv_cb.hwrite <= trans_h.hwrite;
+      vif.drv_cb.hsize <= trans_h.hsize;
+      vif.drv_cb.hburst <= int'(trans_h.hburst_e);
+      vif.drv_cb.htrans <= trans_h.htrans;                       //for single burst type or the first transfer of burst type transaction
+      vif.drv_cb.haddr <= trans_h.haddr_que.pop_front();
+      data_phase_que.push_back(trans_h);
+
+      if(trans_h.calc_txf > 1) begin                             //for burst type transfers
+        for(int i = 0; i < trans_h.calc_txf -1; i++) begin
+          @(vif.drv_cb /*iff vif.drv_cb.hreadyout*/);
+          vif.drv_cb.haddr <= trans_h.haddr_que.pop_front();
+          vif.drv_cb.htrans <= 2'b11;                             //htrans = SEQ 
+      end
+    end
+  end
+  endtask
+
+  task drive_data_signals(AHB_trans trans_h);                                //drives the data signals
+    forever begin
+      wait(data_phase_que.size != 0);                         //checking if the data phase data present or not
+      trans_h = data_phase_que.pop_front();
+      @(vif.drv_cb);
+
+      if(trans_h.hwrite)
+        vif.drv_cb.hwdata <= trans_h.hwdata_que.pop_front();                 //for the single burst type or first transfer of the burst type transaction
+      trans_h.print("Driver");
+      if(trans_h3.calc_txf) begin
+        for(int i=0; i<trans_h3.calc_txf -1;i++) begin
+          @(vif.drv_cb /*iff vif.drv_cb.hreadyout*/);
+          if(trans_h3.hwrite)
+            vif.drv_cb.hwdata <= trans_h3.hwdata_que.pop_front();
+        end
+      end
+    end
+  endtask
 
 endclass
 
